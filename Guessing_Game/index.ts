ca0181d01236685d4e497be70606849b845d4d06
fardan Khan <khanfardan7792@gmail.com>
2024-03-22 23:19:04 +0500

    uploading via git

---
+++ Guessing_Game/index.ts
@@ -0,0 +1,23 @@
+#! usr/bin/env node
+import inquirer from "inquirer";
+
+    let number = Math.floor(Math.random() * 10) + 1;
+
+    while (true) {
+        const guessedNumber = await inquirer.prompt([{
+            message: "Enter the number you've guessed",
+            type: "number",
+            name: "Guess"
+        }]);
+
+        if (isNaN(guessedNumber.Guess)) {
+            console.log("Please enter a number, not an alphabet or special character");
+        } else if (number === guessedNumber.Guess) {
+            console.log("Congratulations, your guess is correct!");
+            break;
+        } else if (guessedNumber.Guess < number) {
+            console.log("TOO LOW!!!");
+        } else {
+            console.log("TOO HIGH!!!");
+        }
+    }
